a072dff07b4c731dbef6c2b9a3c30ae66e032e1b
KmeloR <60187410+KmeloR@users.noreply.github.com>
2020-03-09 10:45:07 -0500

    Add files via upload

---
+++ STRING.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main()
+        {
+            string frase = "the quick brown fox jumps over the lazy dog";
+            frase = frase.ToUpper();
+            string[] palabras = frase.Split(' ');
+            string salida = "the";
+            salida = salida.ToUpper();
+            int repeticion = 0;
+            for (int i = 0; i < palabras.Length; i++)
+            {
+                string palabra = palabras[i];
+                if(palabra ==salida)
+                {
+                    Console.WriteLine(palabra + " encontrada en la posición " + ( i+1));
+                    repeticion++;
+                }
+            }
+            Console.WriteLine("la palabra se repite " + repeticion + " veces");
+        }
+        
+    }
+}
